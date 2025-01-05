9867c21e57e204edcf17b72276699adc3e7b569c
WalaaTurkman <s12323751@stu.najah.edu>
2025-01-05 09:47:51 +0200

    walaaupdatee

--- src/App.js
+++ src/App.js
@@ -3,17 +3,12 @@ import './App.css';
 import Navbar from  './Components/Navbar';
 import Home from  './Pages/Home';
 import ReactDOM from "react-dom";
-import {BrowserRouter, Route,Routes} from "react-router-dom";
-import Footer from './Components/Footer';
 
 
 function App() {
   return (
     <>
     < Navbar/>
-    <Routes> 
-      <Route  path="/" element={<Home/>}/>
-    </Routes>
     <Home />
     
     
